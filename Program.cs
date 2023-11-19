ea219d4248a05bf4c976fb6f5efe87bb1b37af1f
Toni Petrina <tonipetrina@hotmail.com>
2023-11-19 09:41:39 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Load from in-cluster configuration:
+using k8s;
+
+try
+{
+
+    var config = KubernetesClientConfiguration.InClusterConfig();
+
+    // Use the config object to create a client.
+    var client = new Kubernetes(config);
+
+    var namespaces = client.CoreV1.ListNamespace();
+    foreach (var ns in namespaces.Items)
+    {
+        Console.WriteLine(ns.Metadata.Name);
+        var list = client.CoreV1.ListNamespacedPod(ns.Metadata.Name);
+        foreach (var item in list.Items)
+        {
+            Console.WriteLine(item.Metadata.Name);
+        }
+    }
+
+    Console.WriteLine("Done");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Exception caught");
+    Console.WriteLine(ex);
+}
